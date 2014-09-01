d025c184f44ba72908aa825f1003ce420b7f7889
jeffp42ker <jeffp42ker@gmail.com>
2014-08-31 23:59:34 -0400

    A working rotations function w/o helper

--- src/recursion.clj
+++ src/recursion.clj
@@ -64,7 +64,7 @@
               (my-map f (rest seq-1) (rest seq-2)))))
 
 (defn power [n k]
-  (cond (= k 0) 1
+  (cond (= k  0) 1
         :else (* n (power n (dec k)))))
 
 (defn fib [n]
@@ -75,45 +75,56 @@
 (defn my-repeat [how-many-times what-to-repeat]
   (cond (> how-many-times 0)
         (cons what-to-repeat
-              (my-repeat (dec how-many-times) what-to-repeat))))
+              (my-repeat (dec how-many-times) what-to-repeat))
+        :else ()))
 
 (defn my-range [up-to]
   (cond (= up-to 0) ()
         :else (cons (dec up-to) (my-range (dec up-to)))))
 
 (defn tails [a-seq]
-  (cond (empty? a-seq) (cons a-seq ())
-        :else (cons a-seq (tails (rest a-seq)))))
+  (cond (empty? a-seq) '(())
+        :else (cons (seq a-seq) (tails (rest a-seq)))))
 
 (defn inits [a-seq]
-  (cond (empty? a-seq) (cons () (reverse a-seq))
+  (cond ((empty? a-seq) (reverse a-seq))
         :else (cons (seq a-seq)
-
                     (inits (reverse (rest (reverse a-seq)))))))
 
 (defn rotations-helper-map [a-seq]
   (let [observed-rotations {}]
-    (cond (not (contains? observed-rotations a-seq))))
-    )
+    (cond (not (contains? observed-rotations a-seq))
+          false)))
 
 ;; currently broken
-(defn rotations [a-seq]
-  (cond ())
-  (let [last-first (first (reverse a-seq))]
-    (while (not (= last-first (first a-seq)))
-      (rotations (concat (rest a-seq) (cons (first a-seq) ()))))))
 
-(defn my-frequencies-helper [freqs a-seq]
+(defn rotations [a-seq]
+  (cond (empty? a-seq) '(())
+
+        ; base case
+        (not (seq? (first a-seq)))
+        (let [current-seq (seq a-seq)
+              new-seq (concat (rest current-seq) (cons (first current-seq) ()))]
+          (rotations (cons new-seq ())))
+
+        ; completed
+        (= (count a-seq) (count (first a-seq))) a-seq
+
+        :else
+        (let [current-seq (first a-seq)
+              new-seq (concat (rest current-seq) (cons (first current-seq) ()))]
+          (rotations (cons new-seq a-seq)))))
+
+#_(defn my-frequencies-helper [freqs a-seq]
   (if (empty? a-seq)
-    n
-    (let [new-count (if (= elem (first coll))
+     (let [new-count (if (= elem (first coll))
                       (inc n)
                       n)]
       (count-elem-helper new-count
                          elem
                          (rest coll)))))
 
-(defn my-frequencies [a-seq]
+#_(defn my-frequencies [a-seq]
   (frequencies-helper {} a-seq))
 
 (defn un-frequencies [a-map]
